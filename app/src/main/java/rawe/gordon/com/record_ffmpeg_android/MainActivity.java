431f0912b145e2dd97c6dbeb4d6d2f4e4af84ff5
Gordon Rawe <gordon@dst59582.cn1.global.ctrip.com>
2017-01-24 16:28:32 +0800

    update UI

--- app/src/main/java/rawe/gordon/com/record_ffmpeg_android/MainActivity.java
+++ app/src/main/java/rawe/gordon/com/record_ffmpeg_android/MainActivity.java
@@ -1,13 +1,13 @@
 package rawe.gordon.com.record_ffmpeg_android;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import rawe.gordon.com.androidrecord.activities.NewRecordVideoActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
